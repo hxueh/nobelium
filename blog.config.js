7fc616f5ff25ac17bbb603e24b0ee5adb3a93517
hxueh <hxueh@users.noreply.github.com>
2024-03-28 00:36:51 +0800

    Update blog.config.js
    Update comment

--- blog.config.js
+++ blog.config.js
@@ -38,7 +38,7 @@ const BLOG = {
   },
   comment: {
     // support provider: gitalk, utterances, cusdis
-    provider: '', // leave it empty if you don't need any comment plugin
+    provider: 'cusdis', // leave it empty if you don't need any comment plugin
     gitalkConfig: {
       repo: '', // The repository of store comments
       owner: '',
@@ -51,7 +51,7 @@ const BLOG = {
       repo: ''
     },
     cusdisConfig: {
-      appId: '', // data-app-id
+      appId: 'f4d1b22e-6207-4eb5-896d-d0bce8b8cf86', // data-app-id
       host: 'https://cusdis.com', // data-host, change this if you're using self-hosted version
       scriptSrc: 'https://cusdis.com/js/cusdis.es.js' // change this if you're using self-hosted version
     }
